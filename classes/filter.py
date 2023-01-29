a7c7d03727d9da37afea20c13e2292b1a6b2182a
AhmedHassan178 <87389435+AhmedHassan178@users.noreply.github.com>
2023-01-29 18:25:17 +0200

    add Filter class and save mag and phase res

--- classes/filter.py
+++ classes/filter.py
@@ -7,15 +7,37 @@ class Filter():
     def __init__(self) -> None:
         self.zeros = []
         self.poles = []
-        self.gain = 0
-    def set_zeros(self, zeros):
+        self.gain = 1
+    def set_zeros(self, zeros:list) -> None:
         self.zeros = zeros
-    def set_poles(self, poles):
+    def set_poles(self, poles:list) -> None:
         self.poles = poles
-    def set_gain(self, gain):
+    def set_gain(self, gain:float) -> None:
         self.gain = gain
     def get_response(self) -> tuple:
         frequency, response = signal.freqz_zpk(self.zeros, self.poles, self.gain)
         magnitude = 20*np.log(np.abs(response))
         phase = np.unwrap(np.angle(response))
         return (frequency, magnitude, phase)
+    def plot_response(self, frequency, magnitude,phase):
+        plt.figure()
+        plt.ylabel('dB')
+        plt.xlabel('Frequency')
+        plt.plot(frequency, magnitude)
+        plt.xlim(-0.01,np.pi+0.01)
+        plt.title('Magnitude Response')
+        plt.savefig('./static/magnitude.png')
+        plt.figure()
+        plt.ylabel('Phase')
+        plt.xlabel('Frequency')
+        plt.title('Phase Response')
+        plt.plot(frequency, phase)
+        plt.xlim(-0.01,np.pi+0.01)
+        plt.savefig('./static/phase.png')
+    # def plot_response_phase(self, frequency, phase):
+    #     plt.plot(frequency, phase)
+    #     plt.ylabel('Magnitude [dB]')
+    #     plt.xlabel('Frequency [rad/sample]')
+    #     plt.title('Phase Response')
+    #     plt.xlim(-0.01,np.pi+0.01)
+    #     plt.savefig('./static/phase.png')
