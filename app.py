20f714d327c67a61f11a6792ae632be3339d150c
MahmoudRabea13 <93606378+MahmoudRabea13@users.noreply.github.com>
2023-01-29 20:00:46 +0200

    update app.py

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
+from flask import Flask , request, render_template 
 import os
 import json
-import pandas as pd
-from flask import Flask, render_template, jsonify, request
+
+app = Flask( __name__ )
+
+@app.route("/" ,methods=['POST','GET'])
+def image():
+    if request.method == 'POST':
+        
+        return render_template('index.html')
 
 
-app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    pass
+    else:
+        return render_template('index.html')
+
+
+
+@app.route("/catalog" ,methods=['POST','GET'])
+def catalog():
+    if request.method == 'POST':
+        
+        return render_template('catalog.html')
+
+
+    else:
+        return render_template('catalog.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
