9115bcab33c1c4b468a9ad959cc8a9bdd3333df2
AhmedHassan178 <87389435+AhmedHassan178@users.noreply.github.com>
2023-01-31 17:39:42 +0200

    add zeros poles and response of them

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ app = Flask( __name__ )
 process = Process()
 filter = Filter()
 # process.set_filter([-0.2,-0.5,0.98], [-0.99,0.5,0.2+0.6j], 1)
-process.set_filter([1.3], [], 1)
+# process.set_filter([1.3], [], 1)
 def convert_to_complex(input):
     out = []
-    for i in range(len(input['x'])):
-        out.append(input['x'][i]+input['y'][i]*1j)
+    for i in range(len(input)):
+        out.append(input[i].get('x')+input[i].get('y')*1j)
+    return out
 @app.route("/" ,methods=['POST','GET'])
 def image():
     if request.method == 'POST':
@@ -37,6 +38,11 @@ def filter():
     if request.method == 'POST': 
         zeros = request.json['zeros']
         poles = request.json['poles']
+        # print(zeros[0].get('y'))
+        zeros_out = convert_to_complex(zeros)
+        poles_out = convert_to_complex(poles)
+        print(zeros_out)
+        process.set_filter(zeros_out, poles_out, 1)
         frequency, magnitude,phase = process.get_response()
         response = {'frequency':frequency.tolist(),'magnitude':magnitude.tolist(),'phase':phase.tolist()}
         return json.dumps(response)
@@ -47,7 +53,7 @@ def allpass():
     if request.method == 'POST': 
         value = request.json
         print(value)
-        frequency, magnitude,phase = process.all_pass(float(value))
+        frequency, magnitude,phase = process.all_pass(complex(value))
         response = {'frequency':frequency.tolist(),'phase':phase.tolist()}
         return json.dumps(response)
     else:
@@ -57,7 +63,7 @@ def applyallpass():
     if request.method == 'POST': 
         value = request.json
         print(value)
-        process.add_all_pass(float(value))
+        process.add_all_pass(complex(value))
         frequency, magnitude,phase = process.get_response()
         response = {'frequency':frequency.tolist(),'phase':phase.tolist()}
         return json.dumps(response)
